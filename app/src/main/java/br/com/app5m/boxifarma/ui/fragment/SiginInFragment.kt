37c06b6a56383907fa8cacbe0d272150aecf9caa
joao <joaocapitaneo@gmail.com>
2021-08-18 01:05:48 -0300

    foi

---
+++ app/src/main/java/br/com/app5m/boxifarma/ui/fragment/SiginInFragment.kt
@@ -0,0 +1,18 @@
+package br.com.app5m.boxifarma.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SiginInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_sigin_in, container, false)
+    }
+}
