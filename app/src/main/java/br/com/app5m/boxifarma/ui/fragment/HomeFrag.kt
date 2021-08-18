755f7b3fb177b0a3851be5ec67adb3c053f3f6a1
joao <joaocapitaneo@gmail.com>
2021-08-18 18:43:48 -0300

    foi

--- app/src/main/java/br/com/app5m/boxifarma/ui/fragment/HomeFrag.kt
+++ app/src/main/java/br/com/app5m/boxifarma/ui/fragment/HomeFrag.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import br.com.app5m.boxifarma.R
+import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_sigin_in.*
 
 class HomeFrag : Fragment() {
 
@@ -15,5 +18,14 @@ class HomeFrag : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val navigation = Navigation.findNavController(view)
+        buttonHome.setOnClickListener {
+            navigation?.navigate(R.id.action_siginInFragment_to_loginFrag)
+
+        }
+
     }
 }
