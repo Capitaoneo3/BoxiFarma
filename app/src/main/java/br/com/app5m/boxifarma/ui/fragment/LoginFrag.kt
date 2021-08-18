59fab9be69d48083a3ed9d4387fba1274c83cbd7
joao <joaocapitaneo@gmail.com>
2021-08-18 02:43:38 -0300

    foi

--- app/src/main/java/br/com/app5m/boxifarma/ui/fragment/LoginFrag.kt
+++ app/src/main/java/br/com/app5m/boxifarma/ui/fragment/LoginFrag.kt
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.com.app5m.boxifarma.R
 
-class SiginInFragment : Fragment() {
+class LoginFrag : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_sigin_in, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 }
