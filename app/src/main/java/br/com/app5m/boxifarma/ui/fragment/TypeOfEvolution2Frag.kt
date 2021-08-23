5d5f907dc5f6753e063f605f1c93fb455dc199c6
joao <joaocapitaneo@gmail.com>
2021-08-23 16:33:56 -0300

    foi

---
+++ app/src/main/java/br/com/app5m/boxifarma/ui/fragment/TypeOfEvolution2Frag.kt
@@ -0,0 +1,19 @@
+package br.com.app5m.boxifarma.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.app5m.boxifarma.R
+
+class TypeOfEvolution2Frag : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_type_of_evolution2, container, false)
+    }
+}
